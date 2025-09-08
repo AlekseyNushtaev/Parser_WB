2775f438f484c791fa133cc7d3d0a89a34a66217
Aleksei Nushtaev <nushtaev2110@gmail.com>
2025-09-08 22:39:03 +0500

    fix3

--- parser_wb.py
+++ parser_wb.py
@@ -46,15 +46,15 @@ async def scheduler():
 
                         html = browser.page_source
                         soup = bs4.BeautifulSoup(html, 'lxml')
-                        souptext = soup.find(attrs={"class": "product-page productPage--KE6FH"})
-
-                        print(souptext.prettify())
 
                         # Парсим название товара
                         try:
                             new_name = soup.find('h1').text.strip()
                         except:
-                            new_name = None
+                            try:
+                                new_name = soup.find(attrs={"class": "productTitle--J2W7I"}).text.strip()
+                            except:
+                                new_name = None
 
                         # Парсим цену
                         try:
@@ -62,7 +62,12 @@ async def scheduler():
                             digits = ''.join([c for c in price_text if c.isdigit()])
                             new_price = int(digits) if digits else None
                         except:
-                            new_price = None
+                            try:
+                                price_text = soup.find(attrs={"class": "priceBlockWalletPrice--RJGuT redPrice--iueN6"}).text.strip()
+                                digits = ''.join([c for c in price_text if c.isdigit()])
+                                new_price = int(digits) if digits else None
+                            except:
+                                new_price = None
                         print(new_name)
                         print(new_price)
 
